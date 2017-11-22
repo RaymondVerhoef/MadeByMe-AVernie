7816b44803d1fa8983eb81d7e8d5aeef1f16123e
Raymond Verhoef <raymondverhoef@gmail.com>
2017-11-22 10:26:45 +0100

    Disable related products

--- danlet-child/functions.php
+++ danlet-child/functions.php
@@ -27,50 +27,8 @@ add_action( 'wp_enqueue_scripts', 'enqueue_child_theme_style', 100 );
 
 // END ENQUEUE PARENT ACTION - AVERNIE CUSTOMIZED
 
-// BEGIN HIDE SUBCATEGORIES FORM SHOP PAGE - AVERNIE CUSTOMIZED
+// START Disable related products function
 
-add_filter( 'woocommerce_product_categories_widget_arg', 'exclude_widget_category' );
+remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_related_products', 20 );
 
-function exclude_widget_category( $args ) {
-
-    // Create an array that will hold the ids that need to be excluded
-    $exclude_terms = array();
-
-    // Push the default term that you need to hide 
-    array_push( $exclude_terms, 74 );
-
-    // Find all the children of that term
-    $termchildren = get_term_children( 74, 'product_cat' );
-
-    // Iterate over the terms found and add it to the array which holds the IDs to exclude
-    foreach( $termchildren as $child ) {
-        $term = get_term_by( 'id', $child, 'product_cat' );     
-        array_push( $exclude_terms, $term->term_id );
-    }
-
-    // Finally pass the array
-    $args['exclude'] = $exclude_terms;
-
-    return $args;
-}
-
-// END HIDE SUBCATEGORIES FORM SHOP PAGE - AVERNIE CUSTOMIZED
-
-
-/*
-add_filter( 'woocommerce_product_categories_widget_arg', 'woo_product_cat_widget_args' );
-
-function woo_product_cat_widget_args( $cat_args ) {
-	
-	$cat_args['exclude'] = array('79');
-	
-	return $cat_args;
-}
-*/
-/*
-add_action( 'woocommerce_product_categories_widget_arg', 'return_policy', 20 );
- 
-function return_policy() {
-    echo '<p id="rtrn">TESTTESTTEST</p>';
-}
-/*
+// END Disable related products function
